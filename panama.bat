3fa91d1eee2397f72b540b157b3a1372c4ac273e
keychera <kyogatama@gmail.com>
2025-08-11 15:54:09 +0900

    panama can reach this

--- panama.bat
+++ panama.bat
@@ -1,6 +1,3 @@
-cargo clean
-cargo build
-
 jextract --include-dir target/headers --output java/src --target-package self.chera --library rustyplayground target/headers/lib.h --use-system-load-library
 
 javac --source=22 -d java/classes java/src/self/chera/*.java
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,24 @@
+mod app;
+mod egui_tools;
+
+use winit::event_loop::{ControlFlow, EventLoop};
+
 #[unsafe(no_mangle)]
 pub extern "C" fn hello_world() {
     println!("Hello, world from Rust!");
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn init_ui() {
+    pollster::block_on(run());
+}
+
+async fn run() {
+    let event_loop = EventLoop::new().unwrap();
+
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = app::App::new();
+
+    event_loop.run_app(&mut app).expect("Failed to run app");
 }
